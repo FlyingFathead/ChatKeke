a0080712deb852966be1332204ce0d1b6147812c
FlyingFathead <86568670+FlyingFathead@users.noreply.github.com>
2023-06-03 04:08:08 +0300

    Update fennoweb_local_backend.py

--- fennoweb_local_backend.py
+++ fennoweb_local_backend.py
@@ -112,7 +112,7 @@ def generate_text():
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS')
     return response
 
 if __name__ == '__main__':
