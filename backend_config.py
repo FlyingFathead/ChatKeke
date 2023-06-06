445908e905bb1ede382d210e0e515248c2ec85f1
FlyingFathead <86568670+FlyingFathead@users.noreply.github.com>
2023-06-06 10:53:01 +0300

    Update backend_config.py

--- backend_config.py
+++ backend_config.py
@@ -29,10 +29,12 @@ model.py
 
 chat_prefix = '\n|kysymys|'
 chat_suffix = '\n|vastaus|'
-breakpoints = ['\n','|','|v','|k','|ky','|kys','|kysy', '|kysymys|', '|vastaus|', '---', '###', '|' '<|', '<|endoftext|>']  # Add any other desired breakpoints here
 
 # maximum chat context message history memory
-context_memory_length = 3  # Make sure this is an integer
+context_memory_length = 1  # Make sure this is an integer
 
 # what to answer if the answer is empty
 answer_on_empty = "Anteeksi, nyt en ymmärtänyt. Voisitko täsmentää?"
+
+# pieces of text upon which the model should stop generating
+breakpoints = ['\n','|','|v','|k','|ky','|kys','|kysy', '|kysymys|', '|vastaus|', '---', '###', '|' '<|', '<|endoftext|>']  # Add any other desired breakpoints here
