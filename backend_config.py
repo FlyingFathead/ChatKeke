42dd4290902079c6c3e69a81b30f829018117bbe
FlyingFathead <86568670+FlyingFathead@users.noreply.github.com>
2023-06-06 11:04:23 +0300

    clarifying comments on config params

--- backend_config.py
+++ backend_config.py
@@ -27,14 +27,22 @@ sample.py
 model.py
 """
 
+# chat prefix // the text prefix added _before_ every line that's input to the model
+# (this can be i.e.: `<user>` or `User: `, etc...)
 chat_prefix = '\n|kysymys|'
+
+# chat suffix // the suffix added _after_ every line that's input to the model
+# (this can be i.e.: `<AI>` or `ChatBot: `, etc...)
 chat_suffix = '\n|vastaus|'
 
-# maximum chat context message history memory
+# maximum chat context / message history memory
+# note that the higher the count is, the more computationally intensive the model will be
+# no context memory means that the model will treat each query as a new random input
 context_memory_length = 1  # Make sure this is an integer
 
 # what to answer if the answer is empty
+# you can also set the model to try to regenerate an answer if it returns empty; this may increase response time
 answer_on_empty = "Anteeksi, nyt en ymmärtänyt. Voisitko täsmentää?"
 
-# pieces of text upon which the model should stop generating
+# pieces of text upon which the model should stop generating its answer
 breakpoints = ['\n','|','|v','|k','|ky','|kys','|kysy', '|kysymys|', '|vastaus|', '---', '###', '|' '<|', '<|endoftext|>']  # Add any other desired breakpoints here
